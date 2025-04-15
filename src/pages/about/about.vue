42ff16ed27eabfc7afd4c746d603f713233cbe74
wujun <897415845@qq.com>
2025-04-15 18:24:39 +0800

    feat: init template

---
+++ src/pages/about/about.vue
@@ -0,0 +1,20 @@
+<route lang="json5">
+{
+  style: {
+    navigationBarTitleText: '关于',
+  },
+}
+</route>
+
+<template>
+  <view
+    class="bg-white overflow-hidden pt-2 px-4"
+    :style="{ marginTop: safeAreaInsets?.top + 'px' }"
+  >
+    <view class="text-center text-3xl mt-8">About</view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+const { safeAreaInsets } = uni.getSystemInfoSync()
+</script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,30 @@
+<route lang="json5" type="home">
+{
+  style: {
+    navigationStyle: 'custom',
+    navigationBarTitleText: '首页',
+  },
+}
+</route>
+<template>
+  <view
+    class="bg-white overflow-hidden pt-2 px-4"
+    :style="{ marginTop: safeAreaInsets?.top + 'px' }"
+  >
+    <view class="text-center text-2xl mt-2 mb-8">Home LLMOS</view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { TestEnum } from '@/typings'
+import PLATFORM from '@/utils/platform'
+
+defineOptions({
+  name: 'Home',
+})
+
+const { safeAreaInsets } = uni.getSystemInfoSync()
+onLoad(() => {
+  console.log('Home')
+})
+</script>
